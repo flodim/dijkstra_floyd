8f81254bcc2e10f82d20a8ea43627b8f2baee9b5
Dimitri Lizzi <araxor@gmail.com>
2018-02-26 21:27:18 +0100

    First draw of Dijkstra

--- dijkstra.py
+++ dijkstra.py
@@ -1,24 +1,48 @@
 #!/usr/bin/env python3
 
+from math import inf
 
-def calc_weight():
-    pass
 
-def initialize(graph, vertice):
-    dist = []
-    pred = []
+class Graph:
+    def __init__(self, weights_matrix):
+        self.weights_matrix = weights_matrix
+        self.nb_vertices = len(self.weights_matrix)
+        self.vertices = list(range(self.nb_vertices))
 
-    for v in graph:
-        pass
+    def weight(self, from_vertex, to_vertex):
+        return self.weights_matrix[from_vertex][to_vertex]
 
-def extract_min(priority_queue):
-    pass
+    def get_adj(self, vertex):
+        for v, w in enumerate(self.weights_matrix[vertex]):
+            if w != 0 and w != inf:
+                yield v
 
-def relax(u, v, w):
-    pass
 
-def adjacent(g, u):
-    pass
+def dijkstra(graph: Graph, first_vertex):
+    dist = {vertex: inf for vertex in graph.vertices}
+    dist[first_vertex] = 0
+    pred = {vertex: None for vertex in graph.vertices}
 
-def dijkstra(graph, vertice):
-    pass
+    pq = sorted(dist.keys(), key=dist.get)
+    path = []
+
+    while len(pq) > 0:
+        u = pq.pop(pq.index(min(pq)))
+        path.append(u)
+        for v in graph.get_adj(u):
+            u_plus_weight = graph.weight(u, v)
+            if dist[v] > dist[u] + u_plus_weight:
+                dist[v] = dist[u] + u_plus_weight
+                pred[v] = u
+
+    return pred, dist
+
+
+#                 0    1    2    3    4
+graph = Graph([[  0,   2,   4, inf,   3],  # 0
+               [  2,   0,   8, inf,   1],  # 1
+               [  6,   2,   0,   4,   3],  # 2
+               [  1, inf, inf,   0,   5],  # 3
+               [inf, inf, inf,   1,   0]]) # 4
+
+print(dijkstra(graph, 0))
