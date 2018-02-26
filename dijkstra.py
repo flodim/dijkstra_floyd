1b7939d43cdebfe83ecaad84f13c00a834f58511
Dimitri Lizzi <araxor@gmail.com>
2018-02-26 20:08:54 +0100

    Added dijkstra empty functions

---
+++ dijkstra.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+
+def calc_weight():
+    pass
+
+def initialize(graph, vertice):
+    dist = []
+    pred = []
+
+    for v in graph:
+        pass
+
+def extract_min(priority_queue):
+    pass
+
+def relax(u, v, w):
+    pass
+
+def adjacent(g, u):
+    pass
+
+def dijkstra(graph, vertice):
+    pass
